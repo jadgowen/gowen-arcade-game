f7c518d8fbb7130c858f43e7a63242fd4436552d
Joe Gowen <jadgowen@gmail.com>
2018-10-21 15:47:33 -0600

    Implemented game logic, added scoring, difficulty

--- js/app.js
+++ js/app.js
@@ -1,19 +1,51 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+//Success/Fail score capture
+var Score = function () {
+  this.success = 0;
+  this.fail = 0;
+}
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+Score.prototype.addSuccess = function () {
+  this.success += 1;
+  document.querySelector('.success').innerHTML = this.success;
+};
+
+Score.prototype.addFail = function () {
+  this.fail += 1;
+  console.log(this);
+  document.querySelector('.fail').innerHTML = this.fail;
+};
+
+// Random speed function for enemies
+function randomSpeed(min, max) {
+  return Math.floor((Math.random() * ( max - min + 1 )) + min );
+};
+
+//Column segment height 101px, Row segment defined as 83px in engine.js
+
+// Enemies our player must avoid
+var Enemy = function(x, y) {
+    this.x = x;
+    this.y = y;
+    this.speed = randomSpeed(100, 250);
     this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    this.x += this.speed * dt;
+
+    if (this.x > 101 * 6) {
+      this.x = -101;
+      this.speed = randomSpeed(100, 250);
+    }
+
+//Reduced size for col and row size to emulate actual collision distance
+    if (Math.abs(this.x - player.x) < 80 && Math.abs(this.y - player.y) < 60) {
+        player.reset();
+        score.addFail();
+
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,16 +56,72 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.player = 'images/char-cat-girl.png';
+    this.reset();
+
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Player.prototype.update = function() {
+    if(this.col < 0) {
+      this.col = 0;
+    }
+    if(this.col > 4) {
+      this.col = 4;
+    }
+    if(this.row > 5) {
+      this.row = 5;
+    }
+    if(this.row == 0) {
+      this.reset();
+      score.addSuccess();
+    }
+    this.x = this.col * 101;
+    this.y = (this.row * 83)-20;
+
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.player), this.x, this.y);
+};
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+Player.prototype.handleInput = function (keyPress) {
+  if (keyPress == 'left') {
+    this.col -= 1;
+  }
+  if (keyPress == 'right') {
+    this.col += 1;
+  }
+  if (keyPress == 'up') {
+    this.row -= 1;
+  }
+  if (keyPress == 'down') {
+    this.row += 1;
+  }
+
+};
+
+Player.prototype.reset = function() {
+  this.col = 2;
+  this.row = 5;
+  this.x = this.col * 101;
+  this.y = (this.row * 83)-20;
+};
+
+var allEnemies = [];
+var numEnemies = 3;
+
+for(var i = 0; i < numEnemies; i++) {
+  allEnemies.push(new Enemy(i*101, (i+1)*83-20));
+};
+
+var player = new Player();
+var score = new Score();
 
 
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -44,3 +132,40 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+
+//Ensure all page elements are loaded prior to adding event listeners
+window.onload=function(){
+
+//Change difficulty of game
+  document.getElementById('hard').addEventListener('click', function () {
+    allEnemies = [];
+    numEnemies = 4;
+    for(var i = 0; i < numEnemies; i++) {
+      if (i == 2) {
+        allEnemies.push(new Enemy(i*101, (i+1)*83-20));
+        allEnemies.push(new Enemy(i*101, (i+1)*83-20));
+      }
+      else {
+        allEnemies.push(new Enemy(i*101, (i+1)*83-20));
+      }
+    };
+  });
+
+  document.getElementById('medium').addEventListener('click', function() {
+    allEnemies = [];
+    numEnemies = 3;
+    for(var i = 0; i < numEnemies; i++) {
+      allEnemies.push(new Enemy(i*101, (i+1)*83-20));
+    };
+  });
+
+  document.getElementById('easy').addEventListener('click', function() {
+    allEnemies = [];
+    numEnemies = 2;
+    for(var i = 0; i < numEnemies; i++) {
+      allEnemies.push(new Enemy(i*101, (i+1)*83-20));
+    };
+    
+  });
+};
